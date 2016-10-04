0213d1aff23d5dddee007ed8ac85989d09dfaa82
Dmitriy <D:\The Bat!\Data\Mail>
2016-10-04 20:42:26 +0300

    recieve post from checkout - ok

--- app.rb
+++ app.rb
@@ -8,15 +8,19 @@ get '/' do
 	erb :index
 end
 
-post '/zapros' do
-    @data = params
-
-	erb :show
-end
-
 get '/zapros' do
-    @data = params
-
+	@data = params
 	erb :show
 end
 
+post '/zapros' do
+	messageFile = File.open("./public/log.txt", "a") 
+	params.each do |key,value|
+		messageFile.write "Key:#{key}, value:#{value}\n"
+	end
+	messageFile.write "___________________________________________"
+	messageFile.close
+	
+	erb :show
+
+end
